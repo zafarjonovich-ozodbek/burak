1d9a1d5ecb8645d2e6c6e3c09b23e5ee2e18af7c
ron <abilfattoeyo@gmail.com>
2026-09-21 05:19:46 +0900

    feat: MITASK-L

---
+++ src/server.ts
@@ -0,0 +1,3 @@
+// Architectural pattern: MVC, Dependency Injection(DI), MVP
+
+// Design pattern: Middwara, Decorator
---
+++ train.js
@@ -0,0 +1,13 @@
+//  ============== TASK L: ===============
+
+// Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
+// MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
+
+function reverse(a) {
+  return a
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
+
+console.log(reverse("Never give up!"));
