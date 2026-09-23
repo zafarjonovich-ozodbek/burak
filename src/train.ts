3fdcadbc32f56f542bb4bcf7e96befe4f9181ea2
ron <abilfattoeyo@gmail.com>
2026-09-23 16:53:24 +0900

    feat: create train area

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+//  ============== TASK L: ===============
+
+// Shunday function yozing, u string qabul qilsin va string ichidagi hamma sozlarni chappasiga yozib va sozlar ketma-ketligini buzmasdan stringni qaytarsin.
+// MASALAN: reverseSentence("we like coding!") return "ew ekil gnidoc";
+
+function reverse(a: string) {
+  return a
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
+
+console.log(reverse("Never give up!"));
